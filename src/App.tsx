7cdeb1f041c43fe5d44a7e57ed2480719d016ace
prncdev <princedavi94@gmail.com>
2024-04-11 06:34:45 +0530

    commit from personal account

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
 import './styles/global.css'
 
 function App() {
   return (
-    <div className="App">
+    <main className="App ">
       <h1>Happy Hacking.(:</h1>
-    </div>
+    </main>
   );
 }
 
